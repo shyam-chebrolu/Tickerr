534cfb3f428cecd0b8ebb8eff3964beb14502f61
yxreddy <yxreddy@gmail.com>
2015-04-02 23:35:22 -0400

    js updates

--- apps/Tickerr/common/js/app.js
+++ apps/Tickerr/common/js/app.js
@@ -29,6 +29,20 @@ theApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, 
 						templateUrl : "pages/majorIndexes/majorIndexes.html",
 						controller : 'IndexesController as vm'
 					})
+					.state('US_INDEXES', {
+						parent: 'HOME',
+						url : "/indexes",
+						displayName: "Us Indexes",
+						templateUrl : "pages/majorIndexes/usindexes.html",
+						controller : 'UsIndexesController as vm'
+					})
+					.state('COMMODITIES', {
+						parent: 'HOME',
+						url : "/indexes",
+						displayName: "Commodities",
+						templateUrl : "pages/majorIndexes/commodities.html",
+						controller : 'CommoditiesController as vm'
+					})
 					.state('QUOTES', {
 						parent: 'HOME',
 						url : "/quotes",
@@ -36,12 +50,47 @@ theApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, 
 						templateUrl : "pages/quotes/quotes.html",
 						controller : 'QuotesController'
 					})
+					.state('QUOTES2', {
+						parent: 'HOME',
+						url : "/quotes",
+						displayName: "Quotes",
+						templateUrl : "pages/quotes/quotes2.html",
+						controller : 'Quotes2Controller'
+					})
+					.state('NEWS', {
+						parent: 'HOME',
+						url : "/quotes",
+						displayName: "Quotes",
+						templateUrl : "pages/quotes/news.html",
+						controller : 'NewsController'
+					})
+					.state('HISTORY', {
+						parent: 'HOME',
+						url : "/quotes",
+						displayName: "Quotes",
+						templateUrl : "pages/quotes/history.html",
+						controller : 'HistoryController'
+					})
 					.state('SETTINGS', {
 						parent: 'HOME',
 						url : "/settings",
 						displayName: "Settings",
 						templateUrl : "pages/settings/settings.html",
 						controller : 'SettingsController'
+					})
+					.state('PORTFOLIO', {
+						parent: 'HOME',
+						url : "/portfolio",
+						displayName: "Portfolio",
+						templateUrl : "pages/portfolio/portfolio.html",
+						controller : 'PortfolioController'
+					})
+					.state('PORTFOLIO2', {
+						parent: 'HOME',
+						url : "/test",
+						displayName: "Portfolio2",
+						templateUrl : "pages/test/portfolio2.html",
+						controller : 'Portfolio2Controller'
 					})
 					
 }]);
