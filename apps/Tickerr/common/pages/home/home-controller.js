ab4743f86654300ca51e644c18d5d2716de9e158
Shyam Chebrolu <Shyam.Chebrolu@zenmonics.com>
2015-04-21 23:09:45 -0400

    Quick Search enabled
    Quick Search enabled

--- apps/Tickerr/common/pages/home/home-controller.js
+++ apps/Tickerr/common/pages/home/home-controller.js
@@ -5,3 +5,32 @@
 theApp.controller('HomeController',function($scope, $state) {
 
 });
+
+(function() {
+	"use strict";
+	theApp.controller('HomeController', HomeController);
+
+	HomeController.$inject = ['$scope', '$state', 'dataStashService'];
+
+	function HomeController($scope, $state, dataStashService) {
+		this._dataStashService = dataStashService;
+		this._$scope = $scope;
+		this._$state = $state;
+		
+		this._$scope.showTickerDetails = angular.bind(this, this._showTickerDetails);
+		
+		this._init();
+	}
+	
+	HomeController.prototype._init = function () {
+
+	};
+	
+	HomeController.prototype._showTickerDetails = function () {
+		var self = this;
+		
+		self._dataStashService.setData(self._$scope.ticker);
+		self._$state.go("QUOTES");
+	};
+	
+})($);
