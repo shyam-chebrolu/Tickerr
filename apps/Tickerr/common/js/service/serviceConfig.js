d0fd4a24842ed8faa877bb5c533c151426eaf021
NMACHA <Naresh.Macha@Gmail.com>
2015-04-21 22:45:48 -0400

    Added New Ticker controller to show news
    Added New Ticker controller to show news

--- apps/Tickerr/common/js/service/serviceConfig.js
+++ apps/Tickerr/common/js/service/serviceConfig.js
@@ -10,6 +10,12 @@ ServiceConfig = {
 		QUOTE_PRICES: {
 			Url: "https://finance.google.com/finance/info?client=ig&q={0}&callback=?JSON_CALLBACK",
 			Operation: "JSONP"
+		},
+
+		QUOTE_NEWS: {
+			Url: "https://ajax.googleapis.com/ajax/services/feed/find?v=1.0&q={0}",
+			Operation: "GET"
 		}
+		
 		
 }
