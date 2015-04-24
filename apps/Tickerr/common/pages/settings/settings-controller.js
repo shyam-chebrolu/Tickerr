a75a47936191fd2d98c9aa6dfa7bee16bd968c1f
Shyam Chebrolu <Shyam.Chebrolu@zenmonics.com>
2015-04-23 22:38:54 -0400

    controller added for settings page
    controller added for settings page

--- apps/Tickerr/common/pages/settings/settings-controller.js
+++ apps/Tickerr/common/pages/settings/settings-controller.js
@@ -1,6 +1,38 @@
-/**
- * 
- */
-theApp.controller('SettingsController',function($scope, $ionicSideMenuDelegate, $ionicModal) {
-	
+
+theApp.controller('SettingsController',function($scope, $state) {
+
 });
+
+(function() {
+	"use strict";
+	theApp.controller('SettingsController', SettingsController);
+
+	SettingsController.$inject = ['$scope', '$state', 'storageService'];
+
+	function SettingsController($scope, $state, storageService) {
+		this._storageService = storageService;
+		this._$scope = $scope;
+		this._$state = $state;
+		
+		this._$scope.saveSettings = angular.bind(this, this._saveSettings);
+		
+		this._init();
+	}
+	
+	SettingsController.prototype._init = function () {
+		var settings = {
+				passcode: "true",
+				realtime: "true",
+				backup: "true"
+		};
+		
+		this._$scope.settings = settings;
+	};
+	
+	SettingsController.prototype._saveSettings = function () {
+		var self = this;
+
+		alert(this._$scope.settings.realtime);
+	};
+	
+})($);
